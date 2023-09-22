ad1ae7b481748b70a6658dc97858f4d983cd54f9
Humberto Sandmann <hsandmann@ieee.org>
2023-09-21 22:30:56 -0300

    aula06

--- src/main/java/aula03/MainLista.java
+++ src/main/java/aula03/MainLista.java
@@ -1,9 +1,6 @@
 package aula03;
 
-import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Stack;
 
--- src/main/java/aula03/MainSet.java
+++ src/main/java/aula03/MainSet.java
@@ -1,8 +1,5 @@
 package aula03;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 import java.util.Set;
 import java.util.TreeSet;
 
